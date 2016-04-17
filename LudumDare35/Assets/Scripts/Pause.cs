d0ce2d512f42e1ff68ff58070ead2757d1f0daa7
DuckTieCorpMember <danyshbek1996@gmail.com>
2016-04-17 21:38:34 +0600

    asas

---
+++ LudumDare35/Assets/Scripts/Pause.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pause : MonoBehaviour
+{
+	public GameObject pauseMenu;
+
+	void Update()
+	{
+		if(Input.GetKey(KeyCode.Escape))
+		{
+			pauseMenu.SetActive(true);
+			GameObject go = GameObject.FindGameObjectWithTag("Player");
+			if (go != null) {
+				go.GetComponent<PlayerMovement> ().enabled = false;
+				go.GetComponent<PlayerGrenade> ().enabled = false;
+				go.GetComponent<PlayerShoot> ().enabled = false;
+			}
+			Time.timeScale = 0;
+		}
+	}
+
+	public void Unpause()
+	{
+		pauseMenu.SetActive(false);
+		GameObject go = GameObject.FindGameObjectWithTag("Player");
+		if (go != null) {
+			go.GetComponent<PlayerMovement> ().enabled = true;
+			go.GetComponent<PlayerGrenade> ().enabled = true;
+			go.GetComponent<PlayerShoot> ().enabled = true;
+		}
+		Time.timeScale = 1;
+	}
+
+	public void Exit()
+	{
+		Application.Quit();
+	}
+}
